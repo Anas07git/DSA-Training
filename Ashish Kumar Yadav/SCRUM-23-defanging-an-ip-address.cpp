f9c1b52917a55518746b7ee7b00bdff641632904
Ashish4684 <91828783+Ashish4684@users.noreply.github.com>
2024-07-16 12:28:28 +0530

    SCRUM-23-defanging-an-ip-address

--- Ashish Kumar Yadav/SCRUM-23-defanging-an-ip-address.cpp
+++ Ashish Kumar Yadav/SCRUM-23-defanging-an-ip-address.cpp
@@ -1,9 +1,10 @@
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 string defangIPaddr(string address) {
-   string defanged;
+    std::string defanged;
     for (char c : address) {
         if (c == '.') {
             defanged += "[.]";
