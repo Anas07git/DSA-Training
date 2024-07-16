8c76787dd62558d26a8c7ea632feb7b94326a2a6
sheelamakhil <121079760+sheelamakhil@users.noreply.github.com>
2024-07-16 12:30:26 +0530

    Update SCRUM-17-Reverse-Words-in-a-String.java
    This Code is written in Java.

--- Akhil Sheelam/SCRUM-17-Reverse-Words-in-a-String.java
+++ Akhil Sheelam/SCRUM-17-Reverse-Words-in-a-String.java
@@ -17,6 +17,6 @@ public class ReverseWords {
             }
         }
 
-        return result.toString(); // Return the final result
+        return result.toString(); // Return the final results
     }
 }
